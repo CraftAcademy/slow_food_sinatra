c58ed6d602f7ff1224dc322198a4ba80e07e7805
Raoul DIFFOUO <diraulo@gmail.com>
2016-08-23 23:09:13 +0200

    cleanup controller

--- lib/controller.rb
+++ lib/controller.rb
@@ -5,30 +5,24 @@ require_relative 'helpers/data_mapper'
 require_relative 'helpers/warden'
 require 'pry'
 
-
-
-
-
-
 class SlowFood < Sinatra::Base
   enable :sessions
   register Sinatra::Flash
   register Sinatra::Warden
-  set :session_secret, "supersecret"
+  set :session_secret, 'supersecret'
 
-  #binding.pry
-  #Create a test User
+  # Create a test User
   if User.count == 0
-   @user = User.create(username: "admin")
-   @user.password = "admin"
-   @user.save
+    @user = User.create(username: 'admin')
+    @user.password = 'admin'
+    @user.save
   end
 
   use Warden::Manager do |config|
     # Tell Warden how to save our User info into a session.
     # Sessions can only take strings, not Ruby code, we'll store
     # the User's `id`
-    config.serialize_into_session { |user| user.id }
+    config.serialize_into_session(&:id)
     # Now tell Warden how to take what we've stored in the session
     # and get a User from that information.
     config.serialize_from_session { |id| User.get(id) }
@@ -38,20 +32,24 @@ class SlowFood < Sinatra::Base
                           # attempt authentication. We have to define this later.
                           strategies: [:password],
                           # The action is a route to send the user to when
-                          # warden.authenticate! returns a false answer. We'll show
-                          # this route below.
+                          # warden.authenticate! returns a false answer. We'll
+                          # show this route below.
                           action: 'auth/unauthenticated'
     # When a user tries to log in and cannot, this specifies the
     # app to send the user to.
     config.failure_app = self
   end
 
-  Warden::Manager.before_failure do |env, opts|
+  Warden::Manager.before_failure do |env, _opts|
     env['REQUEST_METHOD'] = 'POST'
   end
 
   get '/' do
     erb :index
+  end
+
+  get '/admin' do
+    erb :index_admin, layout: :admin
   end
 
   get '/auth/login' do
